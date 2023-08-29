dbd8fb81a0d71a9073dfcce9fef0d8f374f14106
Njål Borch <njaal.borch@norut.no>
2023-08-29 12:02:10 +0200

    Changed to correct docker

--- qr/run.sh
+++ qr/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 PWD=`pwd`
-docker run --rm  -it --gpus all -v /home:/home -v /var/www:/var/www -p 8890:8890 transcribe $PWD/ai.py -p 8890 $@
+docker run --rm  -it --gpus all -v /home:/home -v /var/www:/var/www -p 8890:8890 img2vec $PWD/ai.py -p 8890 $@
